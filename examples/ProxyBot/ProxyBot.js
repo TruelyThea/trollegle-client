336bcc1e15dd4e9c7c4352d70d0e5ddb037a55af
Thea <theatruely@gmail.com>
2019-10-14 17:59:57 -0500

    updates to ProxyBot, fixed but with -takeover

--- examples/ProxyBot/ProxyBot.js
+++ examples/ProxyBot/ProxyBot.js
@@ -4,10 +4,36 @@ const fs = require("fs"), path = require("path");
 
 const ProxyBotBehavior = require("./ProxyBotBehavior");
 
+let isReadyForSearch = function(proxy) { 
+    return !proxy.searching && Date.now() - proxy.lastChecked > 15 * 60e3;
+};
+
+// This is sort of arbitray right now:
+// every hour since being checked adds 1 to the priority
+// established sets the priority to max(it's current + 1, 0)
+// ban/died subtracts one from the priority
+let priority = function(proxy) {
+    if (!isReadyForSearch(proxy))
+        return -Infinity;
+    return (Date.now() - proxy.lastChecked) / (60 * 60e3) + proxy.priority;
+};
+
+let updateProxy = function(proxy, event) {
+    proxy.working = (event == "established" || event == "disconnected");
+
+    if (proxy.working) proxy.priority = Math.max(proxy.priority + 1, 0);
+    else if (event != "captcha") proxy.priority -= 1;
+
+    proxy.reason = event;
+
+    return proxy.working;
+};
+
 class ProxyBot extends Bot {
     constructor() {
         super();
         this.proxies = [];
+        this.vanilla = false;
 
         this.load().finally(() => { // search twenty-four at a time
             this.proxies.forEach(function(proxy) {
@@ -22,19 +48,17 @@ class ProxyBot extends Bot {
     }
 
     searchProxy() {
-        if (_.all(this.proxies, function(proxy) { return proxy.searching || Date.now() - proxy.lastChecked < 15 * 60e3; }) ||
-                !this.user || !this.user.isConnected || !this.isAdmin()) {
+        let choice = _.max(this.proxies, priority);
+
+        if (!isReadyForSearch(choice)) {
             return new Promise((resolve) => {
                 setTimeout(resolve, 5e3);
             }).then(() => this.searchProxy());
         } else {
-            let proxy = _.min(this.proxies, function(proxy) {
-                return proxy.searching ? Infinity : proxy.lastChecked;
-            });
-            proxy.searching = true;
+            choice.searching = true;
 
             let user = new Connection({
-                proxy: proxy.proxy,
+                proxy: choice.proxy,
                 questionMode: false,
                 lang: "en",
                 proxyMove: false,
@@ -42,22 +66,21 @@ class ProxyBot extends Bot {
             });
 
             return new Promise((resolve, reject) => {
-                user.on("captcha", () => resolve([false, "captcha"]));
-                user.on("ban", () => resolve([false, "ban"]));
-                user.on("died", () => resolve([false, "died"]));
-                user.on("established", () => resolve([true, "success"]));
-                user.on("disconnected", () => resolve([true, "success"]));
-                ["established", "disconnected", "died", "captcha", "ban", "message"].forEach(function(event) {
-                    user.on(event, () => this.logVerbose(proxy.proxy + " " + event));
+                setTimeout(resolve.bind(null, "died"), 5 * 60e3);
+                ["established", "disconnected", "died", "captcha", "ban"].forEach(function(event) {
+                    user.once(event, () => {
+                        this.logVerbose(choice.proxy + " " + event);
+                        resolve(event);
+                    });
                 }, this);
-                
                 user.run();
-            }).then((results) => {
-                if (results[0]) this.say("/!addproxy " + proxy.proxy.slice(9));
-                proxy.lastChecked = Date.now();
-                proxy.searching = false;
-                proxy.working = results[0];
-                proxy.reason = results[1];
+            }).then((event) => {
+                if (updateProxy(choice, event))
+                    this.sendProxy(choice.proxy.slice(9));
+            }).finally(() => {
+                choice.lastChecked = Date.now();
+                choice.searching = false;
+
                 this.save();
                 user.sendDisconnect();
                 user.dispose();
@@ -68,7 +91,7 @@ class ProxyBot extends Bot {
 
     add(proxy) {
         if (!_.findWhere(this.proxies, {proxy: proxy}))
-            this.proxies.push({proxy: proxy, lastChecked: 0, searching: false});
+            this.proxies.push({proxy: proxy, lastChecked: 0, searching: false, priority: 0});
     }
 
     isAdmin() {
@@ -76,10 +99,21 @@ class ProxyBot extends Bot {
        return user && user.flags.indexOf("A") >= 0;
     }
 
+    sendProxy(proxy) {
+        if (this.user && this.user.isConnected)
+            this.say((this.isAdmin() ? "/!addproxy " : "/ hey admin, here is a proxy: ") + proxy);
+        else
+            this.log("Not connected, but found proxy: " + proxy);
+    }
+
     load() {
         return new Promise((resolve, reject) => {
             fs.readFile(path.join(__dirname, "/status.json"), "utf8", (err, data) => {
-                resolve(err ? null : this.proxies = JSON.parse(data));
+                resolve(err ? null : this.proxies = JSON.parse(data).map((proxy) => {
+                    proxy.searching = false;
+                    proxy.priority = proxy.priority || 0;
+                    return proxy;
+                }));
             });
         });
     }
